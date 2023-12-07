402406a83fe8d922537b0d9844c89356f04c6505
ElGhost99 <network.mw99@gmail.com>
2023-12-07 13:58:31 +0100

    Next step of project

--- coffee_machine.py
+++ coffee_machine.py
@@ -1,5 +1,19 @@
+water = int(input("Write how many ml of water the coffe machine has:\n"))
+milk = int(input("Write how many ml of milk the coffe machine has:\n"))
+grams = int(input("Write how many grams of coffe beans the coffe machine has:\n"))
 cups = int(input("Write how many cups of coffee you will need:\n"))
-print(f"For {cups} cups of coffe you will need:")
-print(f"{200*cups} ml of water")
-print(f"{50*cups} ml of milk")
-print(f"{15*cups}  g of coffe beans")
+def check():
+    cups_water = water // 200
+    cups_milk = milk // 50
+    cups_grams = grams // 15
+    cups_max = min(cups_water, cups_milk, cups_grams)
+    return cups_max
+
+cups_max = check()
+if cups ==cups_max:
+    print("Yes, I can make that amount of coffee")
+elif cups < cups_max:
+    more_cups = cups_max - cups
+    print(f"Yes, I can make that amount of coffee (and even {more_cups} more than that)")
+else:
+    print(f"No, I can make only {cups_max} cups of coffee")
